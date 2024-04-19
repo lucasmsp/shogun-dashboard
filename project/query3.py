432f4ccc55c8570f4e596e480f0a6b3367de2c4b
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2024-04-18 22:39:53 -0300

    query3

--- project/query3.py
+++ project/query3.py
@@ -1,9 +1,8 @@
 from dash import html, dcc, dash_table
 import dash_bootstrap_components as dbc
-
+import csv
 from dash.dependencies import Output, Input
-import pandas as pd
-import plotly.express as px
+
 
 INPUT_DATA = 'v3'
 
@@ -11,7 +10,7 @@ INPUT_DATA = 'v3'
 # Precisamos ocultar o "org_list" e de alguma forma, disponibilizar ao usuário, se necessário. P.ex: apenas ao clicar ? por houver (pop-up), exportar como um arquivo csv ?
 # quais gráficos fazer ?
 
-
+# constructs the layout for View 3
 def register_layout_query(dfs):
     # visualização 3
     q3 = [
@@ -19,17 +18,19 @@ def register_layout_query(dfs):
             children=[
                 html.H1(children="View 3 - More details by CVE", className='wrapper'),
                 dbc.Row(
+                    # Renders an interactive table component
                     dash_table.DataTable(
 
                         id='query-3-table',
                         columns=[
                             {"name": i, "id": i} for i in sorted(dfs[INPUT_DATA].columns)
                         ],
+                        hidden_columns=['org_list'],
                         sort_action='custom',
                         sort_mode='multi',
                         sort_by=[],
                         page_current=0,
-                        page_size=10,
+                        page_size=5,
                         style_data={
                             'whiteSpace': 'normal',
                             'height': 'auto',
@@ -41,12 +42,14 @@ def register_layout_query(dfs):
                 ),
                 html.Br(),
                 dcc.Graph(
-                    id="guery-3-graph",
+                    id="query-3-graph",
                     config={
                         'displayModeBar': False,
                         'scrollZoom': True
                     }
-                )
+                ),
+                html.Button('Export csv org-list', id='export-button'),
+                html.Div(id='output-container')
             ]
         )
     ]
@@ -55,6 +58,7 @@ def register_layout_query(dfs):
     return q3
 
 
+# updates the query-3-table data based on user interaction
 def register_callback_query(app, dfs):
     
     @app.callback(
@@ -65,7 +69,7 @@ def register_callback_query(app, dfs):
 
         df = dfs[INPUT_DATA]
         df['org_list'] = df['org_list'].str.join(', ')
-        
+
         if len(sort_by):
             df = df.sort_values(
                 [col['column_id'] for col in sort_by],
@@ -77,4 +81,20 @@ def register_callback_query(app, dfs):
             )
 
         return df.to_dict('records')
-    
+
+    @app.callback(
+        Output('output-container', 'children'),
+        Input('export-button', 'n_clicks')
+    )
+
+    def export_data(n_clicks):
+        if n_clicks:
+            data = dfs[INPUT_DATA]['org_list'].tolist()
+
+            with open('org_list.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['org_list'])
+                for row in data:
+                    writer.writerow([row])
+                return 'Data exported successfully'
+            return ''
