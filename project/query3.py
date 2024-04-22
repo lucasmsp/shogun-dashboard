5a4e62b9a0f562308e5efd60883f3c261b0ef864
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2024-04-21 22:30:05 -0300

    query3

--- project/query3.py
+++ project/query3.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from dash import html, dcc, dash_table
-import dash_bootstrap_components as dbc
-import csv
-from dash.dependencies import Output, Input
 import plotly.express as px
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
+import numpy as np
 
 INPUT_DATA = 'v3'
+
 
 # precisamos add a lógica de filtrar os elementos e outras interatividades: https://dash.plotly.com/datatable/interactivity
 # Precisamos ocultar o "org_list" e de alguma forma, disponibilizar ao usuário, se necessário. P.ex: apenas ao clicar ? por houver (pop-up), exportar como um arquivo csv ?
@@ -24,12 +29,17 @@ def register_layout_query(dfs):
 
                         id='query-3-table',
                         columns=[
-                            {"name": i, "id": i} for i in sorted(dfs[INPUT_DATA].columns)
+                            {"name": i, "id": i, "selectable": True, "deletable": True} for i in
+                            sorted(dfs[INPUT_DATA].columns)
                         ],
+                        editable=True,
+                        filter_action="native",
                         hidden_columns=['org_list'],
                         sort_action='custom',
                         sort_mode='multi',
                         sort_by=[],
+                        row_selectable='multi',
+                        row_deletable=True,
                         page_current=0,
                         page_size=5,
                         style_data={
@@ -42,18 +52,31 @@ def register_layout_query(dfs):
 
                 ),
                 html.Br(),
-                dcc.Graph(
-                    id="query-3-graph",
-                    config={
-                        'displayModeBar': False,
-                        'scrollZoom': True
-                    }
+                # dcc.Graph(
+                #     id="query-3-graph",
+                #     config={
+                #         'displayModeBar': False,
+                #         'scrollZoom': True
+                #     }
+                # ),
+                html.H4('CVE by CVSS, Number of IPs and EPSS Rank'),
+                dcc.Dropdown(
+                    id="dropdown",
+                    options=[
+                        {"label": "CVSS", "value": "cvss"},
+                        {"label": "Number of IPs", "value": "n_ips"},
+                        {"label": "EPSS Rank", "value": "epss_rank"}
+                    ],
+                    value="cvss",
+                    clearable=False,
                 ),
+                dcc.Graph(id="graph"),
             ]
         )
     ]
 
     return q3
+
 
 # register all the callbacks in one place
 def register_callback_query(app, dfs):
@@ -62,7 +85,6 @@ def register_callback_query(app, dfs):
         Input('query-3-table', "sort_by"),
     )
     def update_table3(sort_by):
-
         df = dfs[INPUT_DATA]
         df['org_list'] = df['org_list'].str.join(', ')
 
@@ -79,17 +101,13 @@ def register_callback_query(app, dfs):
         return df.to_dict('records')
 
     @app.callback(
-        Output('query-3-graph', 'figure'),
-        Input('query-3-table', 'data')
-    )
-    def update_graph(data):
-        df = dfs[INPUT_DATA]
-        df['n_orgs'] = df['n_orgs'].astype(int)
-        df['cve_id'] = df['cve_id'].astype(str)
+        Output("graph", "figure"),
+        Input("dropdown", "value"))
 
-        fig = px.bar(df, x='cve_id', y='n_orgs', title='Number of Organizations by CVE')
+    def update_bar_chart(selected_value):
+        df = dfs[INPUT_DATA]
+        colors = px.colors.qualitative.Set1
+        fig = px.bar(df, x="cve_id", y=selected_value, title=f"{selected_value.capitalize()} by CVE", color="n_orgs", color_discrete_sequence=colors)
 
         return fig
-
-
 
