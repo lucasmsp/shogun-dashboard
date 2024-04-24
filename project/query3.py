a270670da0aa8bf7b87ddaa281aa01976a3a17bf
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2024-04-24 17:31:21 -0300

    query3

--- project/query3.py
+++ project/query3.py
@@ -4,17 +4,19 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html, Input, Output
 
-
 INPUT_DATA = 'v3'
 
-
-
-# precisamos add a lógica de filtrar os elementos e outras interatividades: https://dash.plotly.com/datatable/interactivity
 # Precisamos ocultar o "org_list" e de alguma forma, disponibilizar ao usuário, se necessário. P.ex: apenas ao clicar ? por houver (pop-up), exportar como um arquivo csv ?
 # quais gráficos fazer ?
 
 #constructs the layout for View 3
 def register_layout_query(dfs):
+    count_cvss = dfs[INPUT_DATA].groupby('cve_id')['cvss'].count()
+    count_cvss_df = count_cvss.reset_index()
+    count_cvss_df = count_cvss_df.rename(columns={'cvss': 'cvss_count'})
+    dfs[INPUT_DATA] = pd.merge(dfs[INPUT_DATA], count_cvss_df, on='cve_id', how='left')
+
+    dfs[INPUT_DATA]['cvss_count'] = dfs[INPUT_DATA]['cvss_count'].fillna(0)
 
     # visualização 3
     q3 = [
@@ -52,72 +54,26 @@ def register_layout_query(dfs):
                     style={'margin-top': '32px'}
 
                 ),
-                html.Br(),
-                dbc.Row(
-                    children=[
-                        dbc.Col(
-                            children=[
-                                html.H4("Choose input for x-axis"),
-                                dcc.Dropdown(
-                                    id="dropdown1",
-                                    options=[
-                                        {"label": "CVSS", "value": "cvss"},
-                                        {"label": "Number of IPs", "value": "n_ips"},
-                                        {"label": "EPSS Rank", "value": "epss_rank"},
-                                        {"label": "CVSS version", "value": "cvss_version"},
-                                        {"label": "CVSS Rank", "value": "epss_rank"},
-                                        {"label": "Number of orgs", "value": "n_orgs"},
-                                        {"label": "CVE ID", "value": "cve_id"}
-                                    ],
-                                    value="cvss",
-                                    clearable=True,
-                                ),
-                            ],
-                        ),
-                        dbc.Col(
-                            children=[
-                                html.H4("Choose input for y-axis"),
-                                dcc.Dropdown(
-                                    id="dropdown2",
-                                    options=[
-                                        {"label": "CVSS", "value": "cvss"},
-                                        {"label": "Number of IPs", "value": "n_ips"},
-                                        {"label": "EPSS Rank", "value": "epss_rank"},
-                                        {"label": "CVSS version", "value": "cvss_version"},
-                                        {"label": "CVSS Rank", "value": "epss_rank"},
-                                        {"label": "Number of orgs", "value": "n_orgs"},
-                                        {"label": "CVE ID", "value": "cve_id"}
-                                    ],
-                                    value="n_ips",
-                                    clearable=True,
-                                ),
-                            ],
-                        ),
-                    ],
-                    style={'margin-top': '20px'}
-                ),
-                # dcc.Graph(
-                #     id="query-3-graph",
-                #     config={
-                #         'displayModeBar': False,
-                #         'scrollZoom': True
-                #     }
-                # ),
-                dcc.Graph(id="graph"),
+                html.Div(id='datable-interactivity-container')
             ]
         )
     ]
-
+    # count_cvss = dfs[INPUT_DATA].groupby('cve_id')['cvss'].count()
+    # count_cvss_df = count_cvss.reset_index()
+    # count_cvss_df = count_cvss_df.rename(columns={'cvss': 'cvss_count'})
+    # dfs[INPUT_DATA] = pd.merge(dfs[INPUT_DATA], count_cvss_df, on='cve_id', how='left')
+    #
+    # dfs[INPUT_DATA]['cvss_count'] = dfs[INPUT_DATA]['cvss_count'].fillna(0)
     return q3
+
 
 # register all the callbacks in one place
 def register_callback_query(app, dfs):
     @app.callback(
         Output('query-3-table', "data"),
-        Input('query-3-table', "sort_by"),
-        Input('query-3-table', 'filter_query')
+        Input('query-3-table', "sort_by")
     )
-    def update_table3(sort_by, filter_query):
+    def update_table3(sort_by):
         df = dfs[INPUT_DATA]
         df['org_list'] = df['org_list'].str.join(', ')
 
@@ -131,21 +87,57 @@ def register_callback_query(app, dfs):
                 inplace=False
             )
 
-        if filter_query:
-            df = dfs[INPUT_DATA].query(filter_query)
-
         return df.to_dict('records')
 
     @app.callback(
-        Output('graph', 'figure'),
-        [Input('dropdown1', 'value'),
-         Input('dropdown2', 'value'),
-         Input('query-3-table', 'data')]
+        Output('query-3-table', "style_data_conditional"),
+        Input('query-3-table', "sort_by")
     )
+    def update_styles(sort_by):
+        return[{
+            'if': {'column_id': i['column_id']},
+            'background_color': '#D2F3FF'
+        } for i in sort_by]
 
-    def update_bar_chart(x_axis, y_axis, data):
-        df = pd.DataFrame(data)
-        # define paleta de cores
-        colors = px.colors.qualitative.Set1
-        fig = px.scatter(df, x=x_axis, y=y_axis, title=f"{y_axis.capitalize()} vs {x_axis.capitalize()}")
-        return fig
+    @app.callback(
+        Output('datable-interactivity-container', "children"),
+        Input('query-3-table', "derived_virtual_data"),
+        Input('query-3-table', "derived_virtual_selected_rows")
+    )
+    def update_graphs(rows, derived_virtual_selected_rows):
+        if derived_virtual_selected_rows is None:
+            derived_virtual_selected_rows = []
+
+        dff = dfs[INPUT_DATA] if rows is None else pd.DataFrame(rows)
+
+        colors = ['#FBF301' if i in derived_virtual_selected_rows else '#0074D9'
+                  for i in range(len(dff))]
+
+        return[
+            dcc.Graph(
+                id=column,
+                figure={
+                    "data": [
+                        {
+                            "x": dff["cve_id"],
+                            "y": dff[column],
+                            "type": "bar",
+                            "marker": {"color": colors},
+                        }
+                    ],
+                    "layout": {
+                        "xaxis":{"automargin": True},
+                        "yaxis": {
+                            "automargin": True,
+                            "title": {"text": column}
+                        },
+                        "height": 250,
+                        "margin": {"t": 10, "l": 10, "r": 10},
+                    },
+                },
+            )
+            for column in ["cvss", "cvss_rank", "cvss_version", "epss_rank", "n_ips", "n_orgs"] if column in dff
+        ]
+
+
+
